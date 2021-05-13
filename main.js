bfeb50b1528e914584fb094bc67207a1048a3a3e
merlinjones10 <merlinjones10@gmail.com>
2021-05-13 15:51:33 +0100

    first tasks done

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const YOUR_APP_ID = "602bba76";
+const YOUR_APP_KEY = "fe81edebaa638ed7c2cc3086460ee681";
+const recipeLabel = document.querySelector("#recipe-label");
 let foodToSearch = null;
 
 function handleRecipeClick() {
@@ -10,5 +13,16 @@ function handleFoodChange() {
 
 async function fetchRecipe(food) {
   //--- write your code below ---
+  let requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+  const response = await fetch(requestUrl);
+  const data = await response.json();
+  console.log(data.hits[0].recipe.url);
+  recipeLabel.innerHTML = data.hits[0].recipe.url;
   //--- write your code above ---
 }
+
+fetchRecipe();
+
+//Codes
+// 602bba76
+// fe81edebaa638ed7c2cc3086460ee681
