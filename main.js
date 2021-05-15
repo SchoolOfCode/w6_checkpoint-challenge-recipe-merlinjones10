790e76c5c1abd925caccf3acaa913ac5823575b1
merlinjones10 <merlinjones10@gmail.com>
2021-05-15 17:44:55 +0100

    recipe cards with photo and link working

--- main.js
+++ main.js
@@ -1,23 +1,48 @@
 const YOUR_APP_ID = "602bba76";
 const YOUR_APP_KEY = "fe81edebaa638ed7c2cc3086460ee681";
-const recipeLabel0 = document.querySelector("#recipe-label");
-const recipeLabel1 = document.querySelector("#recipe-label2");
-const recipeLabel2 = document.querySelector("#recipe-label3");
-const recipeLabel3 = document.querySelector("#recipe-label4");
-const recipeLabel4 = document.querySelector("#recipe-label5");
 
-const listItem0 = document.querySelector("#li1");
-const listItem1 = document.querySelector("#li2");
-const listItem2 = document.querySelector("#li3");
-const listItem3 = document.querySelector("#li4");
-const listItem4 = document.querySelector("#li5");
+const recipeLink0 = document.querySelector("#recipe-linkl");
+const recipeLink1 = document.querySelector("#recipe-link2");
+const recipeLink2 = document.querySelector("#recipe-link3");
+const recipeLink3 = document.querySelector("#recipe-link4");
+const recipeLink4 = document.querySelector("#recipe-link5");
+const recipeLink5 = document.querySelector("#recipe-link6");
 
-let recipes = [recipeLabel0, recipeLabel1, recipeLabel2, recipeLabel3, recipeLabel4];
-let listItems = [listItem0, listItem1, listItem2, listItem3, listItem4];
+const article0 = document.querySelector("#article1");
+const article1 = document.querySelector("#article2");
+const article2 = document.querySelector("#article3");
+const article3 = document.querySelector("#article4");
+const article4 = document.querySelector("#article5");
+
+const recipeTitle0 = document.querySelector("#recipeTitle1");
+const recipeTitle1 = document.querySelector("#recipeTitle2");
+const recipeTitle2 = document.querySelector("#recipeTitle3");
+const recipeTitle3 = document.querySelector("#recipeTitle4");
+const recipeTitle4 = document.querySelector("#recipeTitle5");
+const recipeTitle5 = document.querySelector("#recipeTitle6");
+
+const img0 = document.querySelector("#img1");
+const img1 = document.querySelector("#img2");
+const img2 = document.querySelector("#img3");
+const img3 = document.querySelector("#img4");
+const img4 = document.querySelector("#img5");
+const img5 = document.querySelector("#img6");
+
+const results = document.querySelector("#results");
+results.innerText = "beans";
+
+let recipes = [recipeLink0, recipeLink1, recipeLink2, recipeLink3, recipeLink4, recipeLink5];
+let articles = [article0, article1, article2, article3, article4];
 let foodToSearch = null;
-
-// listItem1.style.listStyleImage =
-//   "url('https://www.edamam.com/web-img/53c/53ca837dcd939671920e6ab70ad723a6.jpg')";
+let images = [img0, img1, img2, img3, img4, img5];
+let cardTitles = [
+  recipeTitle0,
+  recipeTitle1,
+  recipeTitle2,
+  recipeTitle3,
+  recipeTitle4,
+  recipeTitle5,
+];
 
 function handleRecipeClick() {
   fetchRecipe(foodToSearch);
@@ -27,29 +52,18 @@ function handleFoodChange() {
   foodToSearch = document.querySelector("#food-input").value;
 }
 
-let images = [];
+// let images = [];
 async function fetchRecipe(food) {
-  //--- write your code below ---
+  results.innerHTML = `Showing results for: ${food}`;
   let requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
   const response = await fetch(requestUrl);
   const data = await response.json();
-  console.log(data);
 
-  for (i = 0; i < 5; i++) {
+  for (i = 0; i < recipes.length; i++) {
     recipes[i].innerHTML = data.hits[i].recipe.url;
     recipes[i].href = data.hits[i].recipe.url;
-    images.push(data.hits[i].recipe.image);
-    listItems[i].style.backgroundImage = `url("${images[i]}")`;
+    images[i].src = data.hits[i].recipe.image;
+    cardTitles[i].innerHTML = data.hits[i].recipe.label;
   }
-  images = [];
-
-  // listItems.forEach((element) => {
-  //   element.style.listStyleImage = sampleImage;
-  // });
-  //--- write your code above ---
+  console.log(data);
 }
-
-//Codes
-// 602bba76
-// fe81edebaa638ed7c2cc3086460ee681
-//sample image url : "https://www.edamam.com/web-img/16c/16c9f7a2bcf8a7643ada8729cd693c9d.jpg"
